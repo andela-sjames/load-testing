9871c87ad04e08c1e2358fe5504d37e6bbd1d849
andela-sjames <samuel.james@andela.com>
2018-01-21 00:21:53 +0100

    Add dotenv config to locust.py file

--- README.md
+++ README.md
@@ -10,3 +10,6 @@ First create a python 3 virtual environment inside the repo: `virtualenv -p pyth
 Then, as usual, activate the environment with `source bin/activate`.
 
 Install dependencies with `pip install -r requirements.txt`.
+
+
+To start up locust, simply run `locust`. Then navigate to the web interface at http://127.0.0.1:8089/, and start swarming!
---
+++ locustfile.py
@@ -0,0 +1,38 @@
+import os
+
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+from locust import HttpLocust, TaskSet, task
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+
+email = os.environ.get("email")
+password = os.environ.get("password")
+
+class UserBehavior(TaskSet):
+    def on_start(self):
+        """ on_start is called when a Locust start before any task is scheduled """
+        self.login()
+
+    def login(self):
+        self.client.post("/login", {"email":email, "password":password})
+
+    @task(2)
+    def index(self):
+        self.client.get("/")
+
+    @task(1)
+    def profile(self):
+        self.client.get("/bucketlist/samuel/")
+
+    def single_bucketlist(self):
+        self.client.get("/bucketlist/view/49/")
+
+class WebsiteUser(HttpLocust):
+    host = "https://moments-bucketlist.herokuapp.com"
+    task_set = UserBehavior
+    min_wait = 5000
+    max_wait = 9000
